24cf1556154f1ff305c2bdcaff74cc4867803d74
Rafael Nowrotek <mail@benignware.com>
2015-05-21 17:09:56 +0200

    Updated build tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,104 @@
-var merge = require("deepmerge");
-var path = require('path');
-
 module.exports = function(grunt) {
  
   var pkg = grunt.file.readJSON('package.json');
+  var environment = grunt.option("environment")  || 'development';
+  
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-template');
+  grunt.loadNpmTasks('grunt-curl');
+  grunt.loadNpmTasks('grunt-sync');
+  grunt.loadNpmTasks('grunt-php');
+  grunt.loadNpmTasks('grunt-shell');
+  
+  grunt.loadNpmTasks('grunt-bowerrc');
+  grunt.loadNpmTasks('grunt-mincerrc');
+  
   grunt.initConfig({
+    phpbin: "/Applications/MAMP/bin/php/php5.6.2/bin/php",
     dist: grunt.option('output') || 'dist',
     pkg: pkg,
     clean: {
       'dist': "<%= dist %>",
-      'tmp': ['tmp']
+      'tmp': ['tmp'],
+      'assets': "<%= dist %>/wp-content/themes/**/_assets"
     },
-    copy: {
+    sync: {
       'dist': {
         dot: true,
-        expand: true, 
+        expand: true,
         cwd: '.',
         src: ['app/**/*', 'wp/**/*', 'vendor/**/*', 'index.php', 'wp-config.php', '.htaccess'], 
         dest: grunt.option('output') || "<%= dist %>"
+      },
+      'wordpress': {
+        dot: true,
+        expand: true, 
+        cwd: 'vendor/lib/wordpress',
+        src: ['**/*', '!wp-content'], 
+        dest: grunt.option('output') || "<%= dist %>"
+      },
+      'app': {
+        dot: true,
+        expand: true, 
+        cwd: 'app',
+        src: ['**/*'], 
+        dest: grunt.option('output') && (grunt.option('output') + "/wp-content") || "<%= dist %>/wp-content"
+      },
+      'themes': {
+        dot: true,
+        expand: true, 
+        cwd: 'app/themes',
+        src: ['**/*'], 
+        dest: grunt.option('output') && (grunt.option('output') + "/wp-content/themes") || "<%= dist %>/wp-content/themes"
+      },
+      'languages': {
+        dot: true,
+        expand: true, 
+        cwd: 'app/languages',
+        src: ['**/*'], 
+        dest: grunt.option('output') && (grunt.option('output') + "/wp-content/languages") || "<%= dist %>/wp-content/languages"
+      },
+      'plugins': {
+        dot: true,
+        expand: true, 
+        cwd: 'app/plugins',
+        src: ['**/*'], 
+        dest: grunt.option('output') && (grunt.option('output') + "/wp-content/plugins") || "<%= dist %>/wp-content/plugins"
+      },
+      'mu-plugins': {
+        dot: true,
+        expand: true, 
+        cwd: 'app/mu-plugins',
+        src: ['**/*'], 
+        dest: grunt.option('output') && (grunt.option('output') + "/wp-content/mu-plugins") || "<%= dist %>/wp-content/mu-plugins"
+      },
+      'vendor': {
+        dot: true,
+        expand: true, 
+        cwd: 'vendor',
+        src: ['**/*', '!**/.git', '!**/.svn'], 
+        dest: grunt.option('output') && (grunt.option('output') + "/vendor") || "<%= dist %>/vendor"
+      },
+      'files': {
+        dot: true,
+        expand: true, 
+        cwd: '.',
+        src: ['index.php'], 
+        dest: grunt.option('output') || "<%= dist %>"
+      }
+    },
+    shell: {
+      options: {
+          stderr: false
+      },
+      composer_install: {
+        command: 'php composer.phar install'
+      },
+      composer_update: {
+        command: 'php composer.phar update'
       }
     },
     curl: {
@@ -32,29 +113,41 @@ module.exports = function(grunt) {
             data: function() {
               return {
                 pkg: pkg,
-                environment: grunt.option("environment")  || 'development',
-                database: grunt.file.readYAML('config/database.yml')[( grunt.option("environment")  || 'development' )]  
+                environment: environment,
+                salt: grunt.file.readYAML('config/salt.yml')[environment],
+                database: grunt.file.readYAML('config/database.yml')[environment]  
               };
             }
           },
-          src: 'wp-config.php',
+          src: 'config/wp-config.php',
           dest: ( grunt.option('output') || "<%= dist %>" ) + "/wp-config.php"
       }
     },
     php: {
       serve: {
         options: {
-          bin: "/Applications/MAMP/bin/php/php5.4.34/bin/php",
+          bin: "<%= phpbin %>",
           base: 'dist',
-          keepalive: true,
-          open: true
+          keepalive: false,
+          open: true,
+          port: 5053
         }
       }
+    },
+    watch: {
+      files: ['app/**/*', '!app/**/_assets/**/*'],
+      tasks: ['clean:assets', 'mincerrc:themes', 'sync:app']
     },
     'mincerrc': {
       themes: {
         options: {
-          clean: true
+          clean: true,
+          sourceMaps: false,
+          enable: [
+            'autoprefixer'
+          ],
+          jsCompressor: grunt.option('environment') === 'production' ? 'uglify' : '',
+          //cssCompressor: grunt.option('environment') === 'production' ? 'csswring' : ''
         },
         cwd: 'app/themes',
         src: ['**/.mincerrc']
@@ -65,71 +158,34 @@ module.exports = function(grunt) {
         cwd: 'app/themes',
         src: ['**/.bowerrc']
       }
-    },
-    rsync: {
-      options: {
-        src: "<%= dist %>/.",
-        //args: ["--verbose"],
-        recursive: true
-      },
-      development: {
-        options: {
-          dest: "/Applications/MAMP/htdocs/kicks-app/",
-          //delete: true
-        }
-      },
-      test: {
-        options: {
-          dest: "/var/www/site",
-          host: "user@staging-host",
-          //delete: true // Careful this option could cause data loss, read the docs!
-        }
-      },
-      production: {
-        options: {
-          dest: "/var/www/site",
-          host: "user@live-host",
-          //delete: true // Careful this option could cause data loss, read the docs!
-        }
-      }
     }
   });
   
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-template');
-  grunt.loadNpmTasks('grunt-curl');
-  grunt.loadNpmTasks('grunt-rsync');
-  
-  grunt.loadNpmTasks('grunt-bowerrc');
-  grunt.loadNpmTasks('grunt-mincerrc');
- 
-  /**
-   * This task downloads the latest wordpress to dist
-   */
-  grunt.registerTask('install', [
-    'clean:tmp',
-    'curl:wordpress',
-    'unzip:wordpress',
-    'copy:wordpress',
-    'clean:tmp'
-  ]);
+  grunt.registerTask('composer', 'Basic composer integration', function() {
+    switch (this.args[0]) {
+      case 'download':
+        grunt.task.run('curl:composer');
+        break;
+      case 'install':
+        grunt.task.run('shell:composer_install');
+        break;
+      case 'update':
+        grunt.task.run('shell:composer_update');
+        break;
+      default:
+        grunt.task.run('curl:composer', 'shell:composer_install');
+    }
+    
+  });
   
   grunt.registerTask('build', [
     'mincerrc',
-    'clean:dist',
-    'copy:dist',
+    'clean:assets',
+    'sync:wordpress',
+    'sync:app',
     'template:dist'
   ]);
   
-  grunt.registerTask('deploy', [
-    'build',
-    'rsync:' + (grunt.option('environment') || 'development')
-  ]);
-  
-  grunt.registerTask('serve', [
-    'php:serve'
-  ]);
+  grunt.registerTask('serve', ['build', 'php:serve', 'watch']);
   
 };
