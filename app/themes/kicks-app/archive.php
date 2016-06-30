3f4158dc92ff2eb73b5f3f62c17644bc101fca7b
Rafael Nowrotek <mail@benignware.com>
2016-06-30 15:51:13 +0200

    Integrate wp_bootstrap_hooks pagination

--- app/themes/kicks-app/archive.php
+++ app/themes/kicks-app/archive.php
@@ -43,7 +43,7 @@ get_header(); ?>
 			endwhile;
 
 			// Previous/next page navigation.
-			the_posts_pagination( array(
+			echo wp_bootstrap_get_the_posts_pagination( array(
 				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
 				'next_text'          => __( 'Next page', 'twentyfifteen' ),
 				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
