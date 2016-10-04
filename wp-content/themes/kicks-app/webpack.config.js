bbe171b7470671c03e01181725d708fc1655634b
Rafael Nowrotek <mail@benignware.com>
2016-10-05 01:08:30 +0200

    Fix gitignore

---
+++ wp-content/themes/kicks-app/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  devtool: "source-map",
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a valid name to reference
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: require.resolve('tether'),
+        loader: 'expose?Tether!expose?$'
+      }
+    ]
+  },
+  externals: {
+      // require("jquery") is external and available
+      //  on the global var jQuery
+      "jquery": "jQuery"
+  },
+  sassLoader: {
+    includePaths: ["./node_modules"]
+  }
+};
