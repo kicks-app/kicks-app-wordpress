4d8043b549c69ab178ab15da4400fc1073028444
Rafael Nowrotek <mail@benignware.com>
2016-10-04 22:35:02 +0200

    Refer to correct bundles

--- wp-content/themes/kicks-app/functions.php
+++ wp-content/themes/kicks-app/functions.php
@@ -10,8 +10,8 @@ function enqueue_scripts() {
 	wp_enqueue_script( 'jquery' );
 	
 	// Enqueue application bundles
- 	wp_enqueue_style( 'main', get_template_directory_uri() . '/_assets/css/main.css');
-	wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main-bundle.js', array( 'jquery'));
+ 	wp_enqueue_style( 'main', get_template_directory_uri() . '/css/index-bundle.css');
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/js/index-bundle.js', array( 'jquery'));
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
