81840034f73be9db4e4be3ae25b3d8b80e2b19f9
Rafael Nowrotek <mail@benignware.com>
2016-10-19 16:18:34 +0200

    Add layout to 404 page

--- wp-content/themes/kicks-app/404.php
+++ wp-content/themes/kicks-app/404.php
@@ -8,27 +8,28 @@
  */
 
 get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentysixteen' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentysixteen' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- .site-main -->
-
-		<?php get_sidebar( 'content-bottom' ); ?>
-
-	</div><!-- .content-area -->
-
-<?php get_sidebar(); ?>
+<div class="row">
+  <div class="col-md-8">
+  	<div id="primary" class="content-area">
+  		<main id="main" class="site-main" role="main">
+  
+  			<section class="error-404 not-found">
+  				<header class="page-header">
+  					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentysixteen' ); ?></h1>
+  				</header><!-- .page-header -->
+  
+  				<div class="page-content">
+  					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentysixteen' ); ?></p>
+  
+  					<?php get_search_form(); ?>
+  				</div><!-- .page-content -->
+  			</section><!-- .error-404 -->
+  
+  		</main><!-- .site-main -->
+  	</div><!-- .content-area -->
+  </div>
+  <div class="col-md-4">
+    <?php get_sidebar(); ?>
+  </div>
+</div>
 <?php get_footer(); ?>
