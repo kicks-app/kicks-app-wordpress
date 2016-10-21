d6e8d42d668b41c6619e23f518cf597020e43878
Rafael Nowrotek <mail@benignware.com>
2016-10-21 23:21:21 +0200

    Remove debug output

--- wp-content/themes/kicks-app/single.php
+++ wp-content/themes/kicks-app/single.php
@@ -12,7 +12,6 @@ get_header(); ?>
 	<div class="col-lg-8">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
-			  SINGLE
 				<?php
 				// Start the loop.
 				while ( have_posts() ) : the_post();
