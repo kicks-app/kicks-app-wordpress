7d1615644b78ac9c979c8e21d98ac4b3195a72cf
Rafael Nowrotek <mailbenignware.com>
2019-01-31 16:31:24 +0100

    Remove inactive code

--- bin/install-wp.sh
+++ bin/install-wp.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env sh
 
-# command: sh -c "sleep 20 && wp core install --path=/var/www/html --url=localhost --title=test --admin_user=test --admin_password=test --admin_email=test@example.com"
-
 # Install WordPress.
 wp core install \
   --title="My Wordpress Site" \
   --admin_user="wordpress" \
   --admin_password="wordpress" \
   --admin_email="admin@example.com" \
-  --url="http://localhost:8030" \
+  --url="http://localhost:8090" \
   --skip-email
 
 # Update permalink structure.
